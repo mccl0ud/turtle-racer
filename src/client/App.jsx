1e2c3af994b67891348247efc489de92925bbdcd
ark234 <ark234@gmail.com>
2018-10-08 15:31:00 -0700

    dummy react-redux app

---
+++ src/client/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './app.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
