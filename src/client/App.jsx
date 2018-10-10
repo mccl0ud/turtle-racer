6643eee3493fcaf3cacfdb1116d458432234639d
ark234 <ark234@gmail.com>
2018-10-10 01:39:03 -0700

    implemented chat rooms and able to have multiple users connect to same room and communicate

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import socketIOClient from 'socket.io-client';
+const io = socketIOClient('http://localhost:3333/lobby');
 
 import './app.css';
 import Login from './containers/Login.jsx';
@@ -18,11 +20,35 @@ const mapDispatchToProps = dispatch => ({
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.handleKeystroke = this.handleKeystroke.bind(this);
+    this.joinRoom = this.joinRoom.bind(this);
+    this.leaveRoom = this.leaveRoom.bind(this);
+  }
+
+  joinRoom() {
+    io.emit('joinRoom', 'race-room');
+  }
+
+  leaveRoom() {
+    io.emit('leaveRoom', 'race-room');
+  }
+
+  handleKeystroke(event) {
+    // console.log('New Keystroke -->', event.target.value);
+    io.emit('keystroke', event.target.value);
+  }
+
+  componentDidMount() {
+    io.on('broadcast', msg => console.log(msg));
   }
 
   render() {
     return (
       <div id="app-container">
+        <input name="keyCapture" type="text" onChange={this.handleKeystroke} />
+        <input name="joinRoom" type="button" value="Join Room" onClick={this.joinRoom} />
+        <input name="leaveRoom" type="button" value="Leave Room" onClick={this.leaveRoom} />
         <h1>Ultimate Turtle Racer</h1>
         <div>{this.props.loginSignupToggle ? <Login /> : <Signup />}</div>
       </div>
